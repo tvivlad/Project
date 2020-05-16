ed96caaf11f224641be901fcb17fb8b9e97aadac
vlad <35404251+tvivlad@users.noreply.github.com>
2020-05-16 16:51:24 +0500

    Добавление тексбокса

---
+++ WindowsFormsApp1/ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Mes
+    {
+        public String libmes()
+        {
+            return "Привет из Библиотеки";
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1.ActiveForm.Text = "Hello World!!!";
+            Mes mes1 = new Mes();
+            Form1.ActiveForm.Text = mes1.libmes();
+            textBox1.Text = "Hello World!!!";
+
+        }
+    }
+}
